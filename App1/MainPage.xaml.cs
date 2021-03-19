3de342fffefef3d2eb2738daaf1c9e855e1c5b03
toan bui <justmindgame1@gmail.com>
2021-03-19 07:07:06 +0700

    UWP2 DATA BINDING

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using App1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Book> Books;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Books = BookManager.GetBooks(); 
+        }
+
+        private void GridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var book = (Book)e.ClickedItem;
+            ResultTextBlock.Text = "Selected " + book.Title;
+
+            //Books.Add(new Book { BookId = 6, Title = "Vuljjjjpate", Author = "Futubbbrum", CoverImage = "Assets/10.png" });
+        }
+    }
+}
---
+++ App1/Models/Book.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string CoverImage { get; set; }
+    }
+
+    public class BookManager
+    {
+        public static ObservableCollection<Book> GetBooks()
+        {
+            var books = new ObservableCollection<Book>();
+
+            books.Add(new Book { BookId = 1, Title = "Vulpate", Author = "Fuum", CoverImage = "Assets/1.png" });
+            books.Add(new Book { BookId = 2, Title = "Vue", Author = "Futm", CoverImage = "Assets/11.png" });
+            books.Add(new Book { BookId = 3, Title = "Vuate", Author = "Fuum", CoverImage = "Assets/12.png" });
+            books.Add(new Book { BookId = 4, Title = "Vute", Author = "Fum", CoverImage = "Assets/10.png" });
+            books.Add(new Book { BookId = 5, Title = "Vue", Author = "Fuum", CoverImage = "Assets/2.png" });
+            books.Add(new Book { BookId = 6, Title = "Vte", Author = "Futum", CoverImage = "Assets/3.png" });
+            books.Add(new Book { BookId = 6, Title = "te", Author = "Fum", CoverImage = "Assets/4.png" });
+            books.Add(new Book { BookId = 6, Title = "Vjte", Author = "Futuum", CoverImage = "Assets/5.png" });
+            books.Add(new Book { BookId = 6, Title = "jjte", Author = "Futum", CoverImage = "Assets/6.png" });
+            books.Add(new Book { BookId = 6, Title = "jte", Author = "Futm", CoverImage = "Assets/7.png" });
+            books.Add(new Book { BookId = 6, Title = "Vjpate", Author = "Fum", CoverImage = "Assets/8.png" });
+            books.Add(new Book { BookId = 6, Title = "Vuje", Author = "Fum", CoverImage = "Assets/9.png" });
+            books.Add(new Book { BookId = 6, Title = "Vuje", Author = "Fum", CoverImage = "Assets/1.png" });
+
+            return books;
+        }
+    }
+}
